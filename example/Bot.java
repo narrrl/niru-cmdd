eccc64a313dc4f3fbab696a131d60eb671b25ffe
Nils Pukropp <nils.pukropp@outlook.de>
2020-11-23 01:25:50 +0100

    updated command context and example with jd4

--- example/Bot.java
+++ example/Bot.java
@@ -1,3 +1,10 @@
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
 import nirusu.nirucmd.CommandContext;
 import nirusu.nirucmd.CommandDispatcher;
 import nirusu.nirucmd.annotation.Command;
@@ -8,33 +15,40 @@ import nirusu.nirucmd.annotation.Command;
 public class Bot {
     private CommandDispatcher dispatcher;
 
-    //TODO: implement with jd4
-    //
-    public Bot() {
-        // some other bot config stuff
-        //
+    public Bot(@Nonnull String token) {
+        // create jd4 client
+        DiscordClient client = DiscordClient.create(token);
+        // connect to gateway
+        GatewayDiscordClient gateway = client.login().block();
+        // create dispatcher
         dispatcher = new CommandDispatcher.Builder()
+            // add package that contains the commands
             .addPackage("nirusu.nirubot.command")
             .build();
-    }
 
+        // create trigge
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
 
-
-    public void onGuildCommand(DummyEvent event) {
-        // context of the command
-        CommandContext ctx = new CommandContext(event, args, Command.Context.GUILD);
-
-        // command would be executed here
-        dispatcher.run(ctx, key);
-    }
-
-
-    public void onPrivateCommand(DummyEvent event) {
-        // context of the command
-        CommandContext ctx = new CommandContext(event, args, Command.Context.PRIVATE);
-
-        // command would be executed here
-        dispatcher.run(ctx, key);
-
+            Message mes = event.getMessage();
+            // get message content
+            String raw = mes.getContent();
+            raw = raw == null ? "" : raw;
+            String prefix = "!";
+            // check if message starts with prefix !
+            if (raw.startsWith(prefix) && raw.length() > prefix.length()) {
+                // create the CommandContext
+                CommandContext ctx = new CommandContext(event);
+                List<String> args = Arrays.asList(raw.split("\\w+"));
+                if (args.size() > 0) {
+                    // get key to trigger command
+                    String key = args.get(0);
+                    // remove the key from the arguments
+                    args.remove(key);
+                    // set arguments for the command context
+                    ctx.setArgs(args);
+                    // run dispatcher
+                    dispatcher.run(ctx, key); }
+            }
+        });
     }
 }
--- src/main/java/nirusu/nirucmd/CommandContext.java
+++ src/main/java/nirusu/nirucmd/CommandContext.java
@@ -1,9 +1,81 @@
 package nirusu.nirucmd;
 
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.channel.Channel;
+import discord4j.core.object.entity.channel.MessageChannel;
+import nirusu.nirucmd.annotation.Command;
+
 /**
- * This class represents the current command context (Context (Guild, Private, ...), Event, Message etc.) 
+ * This class represents the current command context (Context (Guild, Private, ...), Event, Message etc.)
  * and contains useful methods to interact with the bot.
  */
 public class CommandContext {
-    
+
+    private List<String> args;
+    private final Command.Context context;
+    private final MessageCreateEvent event;
+
+    /**
+     * Creates a new {@link CommandContext}.
+     *
+     * @param event represents the current event
+     */
+    public CommandContext(@Nonnull MessageCreateEvent event) {
+        this.event = event;
+        Channel.Type type = event.getMessage().getChannel().block().getType();
+        if (type.equals(Channel.Type.DM)) {
+            context = Command.Context.PRIVATE;
+        } else if (type.equals(Channel.Type.GUILD_TEXT)) {
+            context = Command.Context.GUILD;
+        } else {
+            //TODO: write own exception
+            throw new IllegalArgumentException("Invalid Context");
+        }
+    }
+
+    public void setArgs(@Nonnull List<String> args) {
+        this.args = Collections.unmodifiableList(args);
+    }
+
+
+    /**
+     * Shortcut for a reply with a given message @param message
+     */
+    public void reply(@Nonnull String message) {
+        MessageChannel m = event.getMessage().getChannel().block();
+        m.createMessage(message).block();
+    }
+
+    /**
+     * Checks if the current context is equal to @param context
+     *
+     * @return true if {@link #context} equals @param context
+     */
+    public boolean isContext(Command.Context context) {
+        return this.context.equals(context);
+    }
+
+
+    /**
+     * !!! ARGS IS NOT MODIFIABLE !!!
+     * {@link Collections#unmodifiableList}
+     *
+     * @return list of all arguments
+     */
+    public List<String> getArgs() {
+        return this.args;
+    }
+
+    public boolean isPrivate() {
+        return isContext(Command.Context.PRIVATE);
+    }
+
+    public boolean isGuild() {
+        return isContext(Command.Context.GUILD);
+    }
 }
