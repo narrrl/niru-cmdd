122c5fb4ee75cc0e8f65e01414493f28bdfb6fa3
Nils Pukropp <nils.pukropp@outlook.de>
2020-11-26 02:43:26 +0100

    Updated CommandDispatcher

--- src/main/java/nirusu/nirucmd/CommandDispatcher.java
+++ src/main/java/nirusu/nirucmd/CommandDispatcher.java
@@ -1,11 +1,18 @@
 package nirusu.nirucmd;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.Nonnull;
 
+import org.reflections.Reflections;
+
 import nirusu.nirucmd.annotation.Command;
+import nirusu.nirucmd.exception.NoSuchCommandException;
 
 /**
  * This class handles all commands
@@ -18,14 +25,13 @@ import nirusu.nirucmd.annotation.Command;
  */
 
 public class CommandDispatcher {
-    private List<String> packages;
-    private List<BaseModule> modules;
+    private Set<Class<? extends BaseModule>> modules;
 
     public static class Builder {
-        private List<String> packages;
+        private HashSet<String> packages;
 
         public Builder() {
-            packages = new ArrayList<>();
+            packages = new HashSet<>();
         }
 
         public Builder addPackage(@Nonnull String pkg) {
@@ -39,13 +45,68 @@ public class CommandDispatcher {
     }
 
     private CommandDispatcher(Builder b) {
-        packages = b.packages;
-        modules = new ArrayList<>();
-        //TODO: load modules
+        for (String pkg : b.packages) {
+            Reflections ref = new Reflections(pkg);
+            modules = ref.getSubTypesOf(BaseModule.class);
+        }
     }
 
 
-    public void run(@Nonnull CommandContext ctx, @Nonnull String key) {
-        //TODO: implement
+    public void run(@Nonnull CommandContext ctx, @Nonnull String key)
+            throws NoSuchCommandException {
+        BaseModule module = getModuleWith(key);
+        Method refl = getMethodWith(module, key);
+        module.setCommandContext(ctx);
+        try {
+            refl.invoke(module);
+        } catch (IllegalAccessException |
+                InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException argsE) {
+            System.err.println("Commands shouldn't have parameters");
+            argsE.printStackTrace();
+        }
+    }
+
+    private BaseModule getModuleWith(@Nonnull String key)
+            throws NoSuchCommandException {
+        for (Class<? extends BaseModule> md : modules) {
+            if (hasMethodWith(md, key)) {
+                try {
+                    return md.getConstructor().newInstance();
+                } catch (IllegalAccessException | InstantiationException
+                        | InvocationTargetException | NoSuchMethodException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        throw new NoSuchCommandException();
+    }
+
+    private Method getMethodWith(@Nonnull BaseModule module, @Nonnull String key)
+            throws NoSuchCommandException {
+        for (Method refl : module.getClass().getMethods()) {
+            if (refl.isAnnotationPresent(Command.class)) {
+                for (String k : refl.getAnnotation(Command.class).key()) {
+                    if (k.equals(key)) {
+                        return refl;
+                    }
+                }
+            }
+        }
+        throw new NoSuchCommandException();
+    }
+
+    private boolean hasMethodWith(@Nonnull Class<? extends BaseModule> module, @Nonnull String key) {
+        for (Method refl : module.getClass().getMethods()) {
+            if (refl.isAnnotationPresent(Command.class)) {
+                for (String k : refl.getAnnotation(Command.class).key()) {
+                    if (k.equals(key)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
---
+++ src/main/java/nirusu/nirucmd/exception/NoSuchCommandException.java
@@ -0,0 +1,7 @@
+package nirusu.nirucmd.exception;
+
+public class NoSuchCommandException extends Exception {
+    public NoSuchCommandException() {
+        super();
+    }
+}
