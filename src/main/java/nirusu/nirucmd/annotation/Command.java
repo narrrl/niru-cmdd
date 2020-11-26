cb5121eb7ecc915f15ce147f47c2ea61a6bb6cef
Nils Pukropp <nils.pukropp@outlook.de>
2020-11-26 04:01:44 +0100

    retention and target

--- src/main/java/nirusu/nirucmd/annotation/Command.java
+++ src/main/java/nirusu/nirucmd/annotation/Command.java
@@ -1,12 +1,19 @@
 package nirusu.nirucmd.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * This class contains the metadata for every command.
  *
- * {@link #key} is the string that triggers the command aka the key for the command.
- * {@link #description} is a short description for the command.
+ * {@link #key} is the string that triggers the command aka the key for the
+ * command. {@link #description} is a short description for the command.
  * {@link #context} are the contexts for this command, can contain both.
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Command {
     String[] key();
     String description();
