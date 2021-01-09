496db16536c3b7c984ad573a9ab5ae0ec11942af
Nils Pukropp <nils.pukropp@outlook.de>
2021-01-09 06:21:49 +0100

    updated example

--- example/Bot.java
+++ example/Bot.java
@@ -1,57 +1,43 @@
-import java.util.List;
-
 import javax.annotation.Nonnull;
 import discord4j.core.DiscordClient;
-import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import nirusu.nirucmd.CommandContext;
 import nirusu.nirucmd.CommandDispatcher;
-import nirusu.nirucmd.annotation.Command;
 
 public class Bot {
 
     public Bot(@Nonnull String token) {
-        // create jd4 client
-        DiscordClient client = DiscordClient.create(token);
-        // connect to gateway
-        GatewayDiscordClient gateway = client.login().block();
         // create dispatcher
         dispatcher = new CommandDispatcher.Builder()
                 // add package that contains the commands
                 .addPackage("nirusu.nirubot.command").build();
+        // create jd4 client
+        DiscordClient client = DiscordClient.create(token);
+        // connect to gateway
+        client.login().blockOptional().ifPresent(gateway -> {
+            // create trigge
+            gateway.on(MessageCreateEvent.class).subscribe(event -> {
 
-        // create trigge
-        gateway.on(MessageCreateEvent.class).subscribe(event -> {
-
-            Message mes = event.getMessage();
-            // get message content
-            String raw = mes.getContent();
-            raw = raw == null ? "" : raw;
-            String prefix = "!";
-            // check if message starts with prefix !
-            if (raw.startsWith(prefix) && raw.length() > prefix.length()) {
-                // create the CommandContext
-                CommandContext ctx = new CommandContext(event);
-                List<String> args = new ArrayList<>();
-                Collections.addAll(args, raw.substring(prefix.length()).split("\\s+"));
-                if (args.size() > 0) {
-                    // get key to trigger command
-                    String key = args.get(0);
-                    // remove the key from the arguments
-                    args.remove(key);
-                    // set arguments for the command context
-                    ctx.setArgs(args);
-                    // run dispatcher
-                    try {
-                        dispatcher.run(ctx, key);
-                    } catch (NoSuchCommandException e) {
-                        ctx.reply("Unknown command!");
-                    }
+                Message mes = event.getMessage();
+                // get message content
+                String raw = mes.getContent();
+                raw = raw == null ? "" : raw;
+                String prefix = "!";
+                // check if message starts with prefix !
+                if (raw.startsWith(prefix) && raw.length() > prefix.length()) {
+                    // create args by separating each argument at whitespaces
+                    String[] args = raw.substring(prefix.length()).split("\\s+");
+                    CommandContext ctx = new CommandContext(event);
+                    // set args for command context
+                    ctx.setArgsAndKey(args, args[0], true);
+                    CommandToRun cmd = dispatcher.getCommand(ctx, ctx.getKey());
+                    cmd.run();
                 }
-            }
+            });
+
+            gateway.onDisconnect().block();
         });
 
-        gateway.onDisconnect().block();
     }
 }
--- example/HelpModule.java
+++ example/HelpModule.java
@@ -5,16 +5,14 @@ public class HelpModule extends BaseModule {
 
     @Command(
         key = "ping",
-        description = "Ping the bot",
-        context = {Command.Context.GUILD, Command.Context.PRIVATE})
+        description = "Ping the bot")
     public void ping() {
         ctx.reply("Pong!");
     }
 
     @Command(
         key = "help",
-        description = "Lists usefull information about the bot and its commands",
-        context = {Command.Context.GUILD, Command.Context.PRIVATE})
+        description = "Lists usefull information about the bot and its commands")
     public void help() {
         //TODO implement help command
     }
